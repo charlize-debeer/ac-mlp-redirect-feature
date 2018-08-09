8068a5b847d3c7b1f2107efcc0bf422e623e4d52
Malin <malin.kylegard@angrycreative.se>
2018-08-09 10:50:46 +0200

    Added functions for popup and debug mode

--- includes/class-redirect.php
+++ includes/class-redirect.php
@@ -13,6 +13,8 @@ class Redirect {
 	 * Redirect constructor.
 	 */
 	private function __construct() {
+		add_action( 'wp_enqueue_scripts', [ $this, 'output_esi_locale' ], 20 );
+		add_action( 'wp_footer', [ $this, 'add_popup' ] );
 
 	}
 
@@ -28,5 +30,47 @@ class Redirect {
 
 		return self::$instance;
 	}
+
+
+	/**
+	 * Output ESI Locale. (Header: X-Ac-Debug-Country-Code) if set.
+	 *
+	 * @return string
+	 */
+	public function output_esi_locale() {
+		$debug_country_code = $this->get_debug_country_code();
+		?>
+		<script type='text/javascript'>
+			var iDealGeoRedirectLocale = '<?php echo (
+				$debug_country_code ?
+				$debug_country_code :
+				'<esi:include src="/esi/geoip_country"/>'
+			); ?>';
+		</script>
+		<?php
+
+	}
+
+	/**
+	 * Maybe add the popup to the footer.
+	 */
+	public function add_popup() {
+		include dirname( __FILE__ ) . '/popup.php';
+	}
+
+
+	/**
+	 * Get the debug country code. (Header: HTTP_X_IDEAL_DEBUG_COUNTRY_CODE) if set.
+	 *
+	 * @return string
+	 */
+	protected function get_debug_country_code() {
+		if ( ! empty( $_SERVER['HTTP_X_AC_DEBUG_COUNTRY_CODE'] ) ) {
+			return strtoupper( $_SERVER['HTTP_X_AC_DEBUG_COUNTRY_CODE'] );
+		}
+		return false;
+	}
+
+
 
 }
