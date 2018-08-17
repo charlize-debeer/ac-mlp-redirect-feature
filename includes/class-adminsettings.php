1c4828068358a079e6b8fab14c9efe0cd1e2824d
Richard Sweeney <richard.sweeney@angrycreative.se>
2018-08-17 14:34:24 +0200

    Tidy up the settings class

--- includes/class-adminsettings.php
+++ includes/class-adminsettings.php
@@ -1,6 +1,5 @@
 <?php
 /**
- * Adminsetting_sectionOptions
  *
  * @package Ac_Geo_Redirect
  */
@@ -8,7 +7,7 @@
 namespace Ac_Geo_Redirect;
 
 /**
- * Class AdminBase
+ * Class AdminSettings
  *
  * @package Ac_Geo_Redirect\Forms
  */
@@ -17,7 +16,7 @@ class AdminSettings {
 	/**
 	 * Singlenton.
 	 *
-	 * @var AdminForm
+	 * @var AdminSettings
 	 */
 	static protected $single;
 
@@ -39,12 +38,11 @@ class AdminSettings {
 	 * AdminBase constructor.
 	 */
 	public function __construct() {
-
-		register_setting( 'ac_geo_redirect_settings_page', 'agr_options', [ $this, 'agr_validate_options' ] );
-		add_action( 'admin_init', [ $this, 'cookies_initialize_setting_section_options' ] );
+		register_setting( 'ac_geo_redirect_settings_page', 'agr_options', [ $this, 'validate_options' ] );
+		add_action( 'admin_init', [ $this, 'initialize_setting_section_options' ] );
 		add_action( 'admin_menu', [ $this, 'setting_section_menu' ] );
-		$this->options = get_option( 'agr_options' );
 
+		$this->options = get_option( 'agr_options' );
 	}
 
 	/**
@@ -84,19 +82,13 @@ class AdminSettings {
 	 * Markup output.
 	 */
 	public function agr_options_display() {
-
-		$allow_edition = false;
-
-		$page_title = get_admin_page_title();
-
-		if ( current_user_can( 'manage_options' ) ) {
-			$allow_edition = true;
+		if ( ! current_user_can( 'manage_options' ) ) {
+			return;
 		}
 		?>
 
 		<div class="wrap">
-
-			<h2><?php echo $page_title; // WPCS: XSS ok. ?></h2>
+			<h2><?php echo get_admin_page_title(); // WPCS: XSS ok. ?></h2>
 
 			<form method="post" action="options.php">
 				<?php
@@ -111,27 +103,21 @@ class AdminSettings {
 	/**
 	 * Define settings.
 	 */
-	public function cookies_initialize_setting_section_options() {
-
+	public function initialize_setting_section_options() {
 		add_settings_section( 'setting_section', esc_html__( 'AC Geo Redirect Settings', 'ac-geo-redirect' ), [ $this, 'settings_section_callback' ], 'ac_geo_redirect_settings_page' );
 
 		add_settings_field( 'header', esc_html__( 'Header:', 'ac-geo-redirect' ), [ $this, 'header_text_callback' ], 'ac_geo_redirect_settings_page', 'setting_section' );
-
 		add_settings_field( 'subHeader', esc_html__( 'Subheader:', 'ac-geo-redirect' ), [ $this, 'sub_header_text_callback' ], 'ac_geo_redirect_settings_page', 'setting_section' );
-
 		add_settings_field( 'takeMeTo', esc_html__( 'Take me to:', 'ac-geo-redirect' ), [ $this, 'take_me_to_callback' ], 'ac_geo_redirect_settings_page', 'setting_section' );
-
 		add_settings_field( 'remainOn', esc_html__( 'Remain on page:', 'ac-geo-redirect' ), [ $this, 'remain_on_callback' ], 'ac_geo_redirect_settings_page', 'setting_section' );
-
 	}
-
 
 	/**
 	 * Settings Section
 	 */
 	public function settings_section_callback() {
-
 	}
+
 	/**
 	 * Close Button Type.
 	 */
@@ -165,21 +151,25 @@ class AdminSettings {
 	 *
 	 * @param array $input Input post.
 	 *
-	 * @return mixed|void
+	 * @return mixed
 	 */
-	public function agr_validate_options( array $input ) {
+	public function validate_options( array $input ) {
 		if ( ! empty( $input['import'] ) ) {
 			$input = unserialize( $input['import'] );
 		}
+
 		if ( empty( $input['cookie_granularity_settings'] ) ) {
 			array_walk_recursive(
 				$input, function ( &$item, $key ) {
 					$item = sanitize_textarea_field( $item );
 				}
 			);
+
 		}
+
 		$output = wp_parse_args( $input, $this->options );
-		return apply_filters( 'agr_validate_options', $output );
+
+		return apply_filters( 'ac_geo_redirect_validate_options', $output );
 	}
 
 }
